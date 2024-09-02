97cc2050f16f1e92a34cee1dcb895131ee648b1f
AnuAntony888 <websoullabsanu1@gmail.com>
2024-09-02 18:38:34 +0530

    add all

--- utils/db.js
+++ utils/db.js
@@ -11,7 +11,7 @@
 //   host: 'localhost',
 
 //   user: 'vedahtee_smartbilling',
-//   password: 'Admin#2018@',
+  // password: "CezmHV6qQ6}H",
 //   database: 'vedahtee_smartbilling',
 
 // }
@@ -55,21 +55,17 @@
 //     }
 //   }
 // };
- const mysql = require("mysql");
+require('dotenv').config(); // Load environment variables from .env file
+
+const mysql = require('mysql');
 
 const dbConfig = {
-  host: "localhost",
-  port:3306,
-  user: "vedahtee_smartbilling",
-  password: "CezmHV6qQ6}H",
-  database: "vedahtee_smartbilling",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 };
-// const dbConfig = {
-//   host: 'localhost',
-//   user: 'root',
-//   password: 'kzLVN9bMAX/!Ym1h',
-//   database: 'mydatabase'
-// };
 
 let connection;
 
@@ -78,16 +74,16 @@ function handleDisconnect() {
 
   connection.connect((err) => {
     if (err) {
-      console.error("Error connecting to database:", err);
+      console.error('Error connecting to database:', err);
       setTimeout(handleDisconnect, 2000); // Retry after 2 seconds
     } else {
-      console.log("Connected to the database.");
+      console.log('Connected to the database.');
     }
   });
 
-  connection.on("error", (err) => {
-    console.error("Database error:", err);
-    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+  connection.on('error', (err) => {
+    console.error('Database error:', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
       handleDisconnect(); // Reconnect
     } else {
       throw err;
@@ -99,13 +95,13 @@ function handleDisconnect() {
 handleDisconnect();
 
 // Handle termination signals to close the connection gracefully
-process.on("SIGINT", () => {
+process.on('SIGINT', () => {
   if (connection) {
     connection.end((err) => {
       if (err) {
-        console.error("Error closing the connection:", err);
+        console.error('Error closing the connection:', err);
       } else {
-        console.log("Database connection closed.");
+        console.log('Database connection closed.');
       }
       process.exit(0);
     });
