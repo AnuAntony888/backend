cea2fbf288990a558e9e81cfe88b47d539a890e3
AnuAntony888 <websoullabsanu1@gmail.com>
2024-09-02 12:12:52 +0530

    add all

--- utils/db.js
+++ utils/db.js
@@ -55,12 +55,11 @@
 //     }
 //   }
 // };
-
  const mysql = require("mysql");
-// const mysql = require('mysql2');
+
 const dbConfig = {
   host: "localhost",
-  port: 3306,
+  port:3306,
   user: "vedahtee_smartbilling",
   password: "Admin#2018@",
   database: "vedahtee_smartbilling",
