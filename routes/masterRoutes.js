209f67059b1293a3bd33a99919474aec3a7b6b91
AnuAntony888 <websoullabsanu1@gmail.com>
2024-09-04 11:24:36 +0530

    add all

--- routes/masterRoutes.js
+++ routes/masterRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const masterController = require("../controllers/masterController");
 const authenticateToken = require('../middleware/authenticateToken');
 
-router.post('/createmaster', authenticateToken, masterController.createmaster);
+router.post('/createmaster',  masterController.createmaster);
 router.post('/getmasterbyname', authenticateToken, masterController.getmasterByname);
 router.get('/getAllMaster',  masterController.getAllMaster);
 console.log(authenticateToken, "authenticateToken");
